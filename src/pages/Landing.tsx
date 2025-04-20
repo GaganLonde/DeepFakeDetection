2757dffd707e5b0dc08a0351a39d18f61999f5e8
GaganLonde <GaganLonde>
2025-04-20 12:10:48 +0530

    Update footer copyright year in Landing page to 2025

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -153,7 +153,7 @@ const Landing = () => {
       {/* Footer */}
       <footer className="bg-gray-900 text-white py-8">
         <div className="container mx-auto px-4 text-center">
-          <p>&copy; 2024 Deepfake Defender. All rights reserved.</p>
+          <p>&copy; 2025 Deepfake Defender. All rights reserved.</p>
         </div>
       </footer>
     </div>
